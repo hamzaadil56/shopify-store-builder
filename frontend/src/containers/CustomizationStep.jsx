86ddb6f8660abe0b9e37f73d014832cf8c89cbf4
Muhammad Hamza <hamzaadil56@gmail.com>
2024-12-07 22:30:04 +0500

    added form fields

--- frontend/src/containers/CustomizationStep.jsx
+++ frontend/src/containers/CustomizationStep.jsx
@@ -7,15 +7,21 @@ import {
   Stack,
   Button,
   Card,
+  CardHeader,
 } from "@mui/material";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
 
 const CustomizationStep = () => {
   return (
     <div>
       <StepDescriptionCard
-        stepTitle={"Set Access Scopes"}
+        stepTitle={"Customisation"}
         stepDescription={
-          "This step consists in authorizing us to use our AI tools to build and customize your store. You can do so by setting your access scopes, and we'll do the rest. No worries, it takes 2 minutes, and we'll guide you along the way. Simply follow the steps below or watch the explanatory video."
+          "Choose your niche and color scheme, and let our expertise weave them into a stunning online store tailored just for you. Together, we'll craft a digital storefront that not only reflects ypur unique vision but also captivates your audience, ensuring a memorable shopping experience from click to checkout. "
         }
       />
       <Box marginY={4}>
@@ -24,28 +30,194 @@ const CustomizationStep = () => {
             <Typography variant="h6" gutterBottom>
               Follow the steps
             </Typography>
-            <Box sx={{ color: "text.secondary", fontSize: 18 }}>
-              {/* Previous steps remain the same */}
 
-              <Typography
-                sx={{ my: 4 }}
-                variant="p"
-                component={"p"}
-                gutterBottom
+            <Card sx={{ my: 2 }} variant="outlined">
+              <Box
+                sx={{
+                  borderBottom: "1px solid rgba(255, 255, 255, 0.12)",
+                  padding: "10px 16px",
+                  backgroundColor: "#1e1e1f",
+                }}
               >
-                1. In the same Settings section, go to plan. Click on Choose
-                plan, and select your plan. Then, set your payment method.
-              </Typography>
-              <Typography
-                sx={{ my: 4 }}
-                variant="p"
-                component={"p"}
-                gutterBottom
+                <Typography>Select your niche</Typography>
+              </Box>
+              <CardContent sx={{ padding: "24px !important" }}>
+                <FormControl>
+                  <RadioGroup
+                    row
+                    aria-labelledby="demo-row-radio-buttons-group-label"
+                    name="row-radio-buttons-group"
+                  >
+                    <FormControlLabel
+                      value="female"
+                      control={
+                        <Radio sx={{ padding: "0", marginRight: "10px" }} />
+                      }
+                      label="Fitness and Gym"
+                      sx={{
+                        border: "1px solid rgba(255, 255, 255, 0.12)",
+                        padding: "10px 15px",
+                        borderRadius: "8px",
+                        my: 1,
+                      }}
+                    />
+                    <FormControlLabel
+                      value="male"
+                      control={
+                        <Radio sx={{ padding: "0", marginRight: "10px" }} />
+                      }
+                      label="Toys and Babies"
+                      sx={{
+                        border: "1px solid rgba(255, 255, 255, 0.12)",
+                        padding: "10px 15px",
+                        borderRadius: "8px",
+                        my: 1,
+                      }}
+                    />
+                    <FormControlLabel
+                      value="other"
+                      control={
+                        <Radio sx={{ padding: "0", marginRight: "10px" }} />
+                      }
+                      label="Pet Supplies"
+                      sx={{
+                        border: "1px solid rgba(255, 255, 255, 0.12)",
+                        padding: "10px 15px",
+                        borderRadius: "8px",
+                        my: 1,
+                      }}
+                    />
+                    <FormControlLabel
+                      value="other"
+                      control={
+                        <Radio sx={{ padding: "0", marginRight: "10px" }} />
+                      }
+                      label="Beauty"
+                      sx={{
+                        border: "1px solid rgba(255, 255, 255, 0.12)",
+                        padding: "10px 15px",
+                        borderRadius: "8px",
+                        my: 1,
+                      }}
+                    />
+                    <FormControlLabel
+                      value="other"
+                      control={
+                        <Radio sx={{ padding: "0", marginRight: "10px" }} />
+                      }
+                      label="Pick a niche for me"
+                      sx={{
+                        border: "1px solid rgba(255, 255, 255, 0.12)",
+                        padding: "10px 15px",
+                        borderRadius: "8px",
+                        my: 1,
+                      }}
+                    />
+                  </RadioGroup>
+                </FormControl>
+              </CardContent>
+            </Card>
+            <Card variant="outlined">
+              <Box
+                sx={{
+                  borderBottom: "1px solid rgba(255, 255, 255, 0.12)",
+                  padding: "10px 16px",
+                  backgroundColor: "#1e1e1f",
+                }}
               >
-                2. Press Subscribe.
+                <Typography>Select your color scheme</Typography>
+              </Box>
+              <CardContent sx={{ padding: "24px !important" }}>
+                <FormControl>
+                  <RadioGroup
+                    row
+                    aria-labelledby="demo-row-radio-buttons-group-label"
+                    name="row-radio-buttons-group"
+                  >
+                    <FormControlLabel
+                      value="female"
+                      control={
+                        <Radio sx={{ padding: "0", marginRight: "10px" }} />
+                      }
+                      label="Blue"
+                      sx={{
+                        border: "1px solid rgba(255, 255, 255, 0.12)",
+                        padding: "10px 15px",
+                        borderRadius: "8px",
+                        my: 1,
+                      }}
+                    />
+                    <FormControlLabel
+                      value="male"
+                      control={
+                        <Radio sx={{ padding: "0", marginRight: "10px" }} />
+                      }
+                      label="Green"
+                      sx={{
+                        border: "1px solid rgba(255, 255, 255, 0.12)",
+                        padding: "10px 15px",
+                        borderRadius: "8px",
+                        my: 1,
+                      }}
+                    />
+                    <FormControlLabel
+                      value="other"
+                      control={
+                        <Radio sx={{ padding: "0", marginRight: "10px" }} />
+                      }
+                      label="Yellow"
+                      sx={{
+                        border: "1px solid rgba(255, 255, 255, 0.12)",
+                        padding: "10px 15px",
+                        borderRadius: "8px",
+                        my: 1,
+                      }}
+                    />
+                    <FormControlLabel
+                      value="other"
+                      control={
+                        <Radio sx={{ padding: "0", marginRight: "10px" }} />
+                      }
+                      label="Purple"
+                      sx={{
+                        border: "1px solid rgba(255, 255, 255, 0.12)",
+                        padding: "10px 15px",
+                        borderRadius: "8px",
+                        my: 1,
+                      }}
+                    />
+                    <FormControlLabel
+                      value="other"
+                      control={
+                        <Radio sx={{ padding: "0", marginRight: "10px" }} />
+                      }
+                      label="Grey"
+                      sx={{
+                        border: "1px solid rgba(255, 255, 255, 0.12)",
+                        padding: "10px 15px",
+                        borderRadius: "8px",
+                        my: 1,
+                      }}
+                    />
+                  </RadioGroup>
+                </FormControl>
+              </CardContent>
+            </Card>
+            <Stack gap={4} my={4} alignItems={"center"}>
+              <Typography color="text.secondary" component={"p"}>
+                Press the button to start the building process.
               </Typography>
-            </Box>
-
+              <Button variant="contained" sx={{ maxWidth: 400 }}>
+                Build my store
+              </Button>
+              <Typography color="text.secondary" component={"p"} textAlign={'center'}>
+                Please note that the customer reviews displayed on your store
+                are generated by artificial intelligence and do not represent
+                authentic feedback from real customers. These AI-generated
+                reviews are designed for demonstration purposes only and should
+                not be cosnidered as genuine user testimonials.
+              </Typography>
+            </Stack>
             <Stack direction={"row"} justifyContent={"space-between"}>
               <Button color="primary" variant="contained" sx={{ mt: 2 }}>
                 Back
